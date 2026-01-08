845203d74b9e4e9a3858cfc4c8a95adac043b07c
“Anurag <anuragyadav22211@gmail.com>
2026-01-09 02:52:10 +0530

    controllers middlewares models

--- src/index.js
+++ src/index.js
@@ -1,25 +1,59 @@
-import express from "express";
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 
-const app = express();
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
-});
-
-const PORT = process.env.PORT || 3000;
 
 connectDB()
-  .then(() => {
+.then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("DB connection failed", err);
-  });
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// import connectDB from "./db/index.js";
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Backend running 🚀");
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`Server running on port ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("DB connection failed", err);
+//   });
 
 
 
